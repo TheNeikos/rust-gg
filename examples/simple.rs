b56a8c9240ae3cae2f6b75a1c719cb3108aeeef2
Marcel Müller <neikos@neikos.email>
2015-10-12 17:50:30 +0200

    examples: Fix simple example

--- examples/simple.rs
+++ examples/simple.rs
@@ -7,15 +7,15 @@ use gg::event::step::fixed_60;
 fn main() {
     let display = glium::glutin::WindowBuilder::new().build_glium().unwrap();
 
-    // fixed_60(|_dt| {
-    //     use gg::event::StepResult;
-    //     for ev in display.poll_events() {
-    //         match ev {
-    //             glium::glutin::Event::Closed => return StepResult::Stop,
-    //             _ => ()
-    //         }
-    //     }
-    //     return StepResult::Continue;
-    // });
+    fixed_60(|_dt| {
+        use gg::event::StepResult;
+        for ev in display.poll_events() {
+            match ev {
+                glium::glutin::Event::Closed => return StepResult::Stop,
+                _ => ()
+            }
+        }
+        return StepResult::Continue;
+    });
 
 }
