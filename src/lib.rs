8f791e74a9c913b81eb2e561e86284df701d0310
Marcel Müller <neikos@neikos.email>
2015-10-19 16:05:31 +0200

    src: Add Game Struct

--- src/lib.rs
+++ src/lib.rs
@@ -6,8 +6,12 @@
 
 #![deny(missing_docs)]
 
+#![feature(associated_type_defaults)]
+
 extern crate glium;
 extern crate time;
+
+use glium::backend::glutin_backend::GlutinFacade;
 
 /// The event module
 /// TODO: Expand
@@ -15,3 +19,11 @@ pub mod event;
 
 /// The Scenes module
 pub mod scene;
+
+/// The game object, you give it your initial State and start it off
+pub struct Game<T> {
+    /// Your own state
+    state: T,
+    /// The display handle
+    display: GlutinFacade
+}
